9be74a6ce4476b2c5ff38f6a0b45721581470228
Dan Ashton <57535864+danashton1@users.noreply.github.com>
2019-12-24 12:56:07 +0000

    Add files via upload

--- AppPage.js
+++ AppPage.js
@@ -1,7 +1,36 @@
-window.onload=function() {
-    var red = document.getElementById(spinnerRed);
+var order = [];
+var playerOrder = [];
+var playerTurn;
+var compTurn;
+var score;
+var random;
+var lightInterval;
 
-    setTimeout(function(){
-        red
-    }, 1000);
+document.getElementById('startButton').onclick = function() {
+    play();
+};
+function play() {
+ order = [];
+ playerOrder = [];
+ lightInterval = 0;
+
+    for (var i = 0; i < 20; i++) {
+             random = Math.floor(Math.random() * 4) + 1
+            order.push(random);
+        }
+    compTurn = true;
+
+    lightInterval = setInterval(gameturn, 700);
+}
+
+function gameturn() {
+    if(playerTurn){
+        clearInterval(lightInterval);
+        compTurn = false;
+        clearColour();
+    }
+    if(compTurn){
+        clearColour();
+    }
+
 }
